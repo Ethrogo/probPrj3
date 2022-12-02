abb3b64f7632423bc416c87850660a56466a8185
Ehan <ethrogo@gmail.com>
2022-12-02 15:52:47 -0500

    monte carlo designed random values

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    final static long multiplier = 24693; //a
+    final static long increment = 3967;
+    final static long startX = 1000; //x naught // x sub 0
+    final static double modulus = Math.pow(2, 18);    //k
+    public static void main(String[] args) {
+        getAnyRandomValues(1, 2, 3);
+
+    }
+    public static double getAnyX(int i)
+    {   //Conditions: i >= 0
+        if(i >= 1)
+        {
+            return (multiplier * getAnyX(i -1) + increment) % modulus;
+        }
+        return startX;
+    }
+
+    public static double getAnyRandomValue(int i)
+    {
+        return getAnyX(i)/ modulus;
+    }
+
+    public static void getAnyRandomValues(Integer... ints)
+    {
+        for(int each: ints)
+        {
+            System.out.println(getAnyRandomValue(each));
+        }
+    }
+}
